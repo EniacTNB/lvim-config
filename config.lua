c3605766dc52cc71fac893458ac7e4351e50d924
ENIACCC <eniac_z@icloud.com>
2024-04-25 23:45:24 +0800

    Sovle macos conflict

--- config.lua
+++ config.lua
@@ -601,6 +601,47 @@ lvim.plugins = {
                         require("colorful-winsep").setup()
                 end,
         },
+        {
+                "kevinhwang91/nvim-ufo",
+                dependencies = {
+                        "kevinhwang91/promise-async",
+                        {
+                                "luukvbaal/statuscol.nvim",
+                                config = function()
+                                        local builtin = require("statuscol.builtin")
+                                        require("statuscol").setup({
+                                                relculright = true,
+                                                segments = {
+                                                        { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+                                                        { text = { "%s" }, click = "v:lua.ScSa" },
+                                                        { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
+                                                },
+                                        })
+                                end,
+                        },
+                },
+                event = "BufReadPost",
+                opts = {
+                        provider_selector = function()
+                                return { "treesitter", "indent" }
+                        end,
+                },
+
+                init = function()
+                        vim.keymap.set("n", "zR", function()
+                                require("ufo").openAllFolds()
+                        end)
+                        vim.keymap.set("n", "zM", function()
+                                require("ufo").closeAllFolds()
+                        end)
+                end,
+        },
+        -- Folding preview, by default h and l keys are used.
+        -- On first press of h key, when cursor is on a closed fold, the preview will be shown.
+        -- On second press the preview will be closed and fold will be opened.
+        -- When preview is opened, the l key will close it and open fold. In all other cases these keys will work as usual.
+        { "anuvyklack/fold-preview.nvim", dependencies = "anuvyklack/keymap-amend.nvim", config = true },
+        --
 }
 -- lvim.on_load("telescope.nvim", function()
 --                                         require("telescope").load_extension("projects")
@@ -684,6 +725,15 @@ vim.opt.ignorecase = true                       -- 搜索时忽略大小写
 vim.opt.mouse = "a"                             -- 允许lvim使用鼠标操作
 vim.opt.clipboard = "unnamedplus"               -- 使用OS 系统剪贴板
 
+
+
+--- 用来配置折叠相关配置
+-- UFO folding
+vim.o.foldcolumn = "1" -- '0' is not bad
+vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
+vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
 -- lvim.keys.normal_mode["<C-d>"] = "<C-d>zz"      -- Ctrl-d 将光标下翻半页并将光标行在窗口上下居中
 
 -- cscope_maps 插件优先加载当前目录里的cscope.out, 然后加载与.git同级目录里的cscope.out,当只关注kernel的某个子模块目录时, 可以加速搜索
